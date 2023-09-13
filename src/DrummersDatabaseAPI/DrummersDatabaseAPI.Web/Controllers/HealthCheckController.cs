a19acdc3ebc53df1e71f5e504d8c0ad987911bcb
RedBirdOBX <shane.fowlkes.70@gmail.com>
2023-09-13 15:23:16 -0400

    added health check

---
+++ src/DrummersDatabaseAPI/DrummersDatabaseAPI.Web/Controllers/HealthCheckController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DrummersDatabaseAPI.Web.Controllers
+{
+    /// <summary>
+    /// Health Check controller
+    /// </summary>
+    [Route("api/health")]
+    [ApiController]
+    public class HealthCheckController : ControllerBase
+    {
+
+        /// <summary>
+        /// health check endpoint
+        /// </summary>
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public ActionResult HealthCheck()
+        {
+            return Ok("Failure is the opportunity to begin again more intelligently.");
+        }
+    }
+}
